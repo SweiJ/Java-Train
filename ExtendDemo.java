48c6a090d921fea423add3b6d9a6292903e91120
SweiJ <74802750+SweiJ@users.noreply.github.com>
2021-11-30 21:33:36 +0800

    Add files via upload

---
+++ ExtendDemo.java
@@ -0,0 +1,15 @@
+package extend;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: SweiJ
+ * Date: 2021-11-20
+ * Time: 10:41
+ */
+public class ExtendDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
