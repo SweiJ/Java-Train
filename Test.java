6056ad2e51062d7acedffb63d5176ced2ece828d
SweiJ <74802750+SweiJ@users.noreply.github.com>
2021-12-21 23:11:09 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,34 +1,42 @@
-import static java.lang.Math.*;
-
 /**
  * Created with IntelliJ IDEA.
  * Description:
  * User: SweiJ
- * Date: 2021-11-20
- * Time: 19:14
+ * Date: 2021-12-17
+ * Time: 23:05
  */
 public class Test {
-    public static  void heart2(){
-        double x,y,a;
-        char s[]=new char[]{'I',' ','l','o','v','e',' ','y','o','u','!'};
-        int index=0;
-        for(y=1.3f;y>-1.1f;y-=0.06f){
-            index=0;
-            for(x=-1.1f;x<=1.1f;x+=0.025f){
-                double result=x*x+pow((5.0*y/4.0-sqrt(abs(x))),2);
-                if(result<=1){
-                    System.out.print((s[index]));
-                    index=(index+1)%11;
-                }
-                else{
-                    System.out.print(' ');
-                }
-            }
-            System.out.println(" ");
-        }
-    }
-
     public static void main(String[] args) {
-        heart2();
+        shengdanshu();
+    }
+    public static void shengdanshu(){
+        //叶子层
+        int level = 10;
+        //根层
+        int rootLevel = 2;
+        int spaceNum = level - 1;
+
+        //画叶子
+        // 为什么从1开始  不管了就是任性
+        for (int i = 1; i <= level; i++) {
+            spaceNum--;
+            for (int g = 0; g <= spaceNum; g++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= ((i * 2) -1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+        System.out.println("  ?圣诞节快乐?");
+        //画根
+        for (int i = 1; i <= rootLevel; i++) {
+            for (int g = 1; g < level; g++) {
+                System.out.print(" ");
+            }
+            System.out.print("*\n");
+        }
+
     }
 }
