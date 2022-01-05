c965957788e9e96494d9dcd6e0cabac3c6039426
SweiJ <74802750+SweiJ@users.noreply.github.com>
2022-01-05 17:43:49 +0800

    Add files via upload

--- SocketServer.java
+++ SocketServer.java
@@ -1,4 +1,4 @@
-package com.swei.socketTest;
+package com.swei.socketthread;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -14,72 +14,19 @@ import java.net.Socket;
 public class SocketServer {
     public static void main(String[] args) {
         ServerSocket ss = null;
-        Socket accept = null;
-        InputStream is = null;
-        ObjectInputStream ois = null;
-        OutputStream os = null;
-        DataOutputStream dos = null;
+        Socket s = null;
+        int count = 0;
         try {
-            // 服务端使用ServerSocket套接字接收端口号
             ss = new ServerSocket(8888);
-            accept = ss.accept();
-            is = accept.getInputStream();
-            ois = new ObjectInputStream(is);
-            User user = (User) ois.readObject();
-            Boolean flag = false;
-            if("小姜".equals(user.getName()) && "12345".equals(user.getPwd())) {
-                flag = true;
+            while(true) {
+                s = ss.accept();
+                new SocketThreadTest(s).start();
+                count++;
+                System.out.println("第" + count + "个用户访问服务器" + s.getInetAddress());
             }
 
-            os = accept.getOutputStream();
-            dos = new DataOutputStream(os);
-            dos.writeBoolean(flag);
-
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if(dos != null) {
-                    dos.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if(os != null) {
-                    os.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if(is != null) {
-                    is.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if(ois != null) {
-                    ois.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if(accept != null) {
-                    accept.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if(ss != null) {
-                    ss.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
